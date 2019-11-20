7761989502a96e04d660f7e4b8bf485a93d19781
Andrew Sooklall <andrewsooklall@gmail.com>
2019-11-20 18:05:38 -0500

    modified script 1 to allow for blank searches, started script 2 modifications

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,14 +19,16 @@ function handleSubmit() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
   
-    // Select the new input value from the form
+    // Select the new input values from the form
     var inputValue = d3.select("#datetime").property("value");
   
     // clear the table to prepare for new data filter
     tbody.html("")
-  
+
+    // if there is a value being searched:
+if (inputValue){
     // filter the data based on the search
-    var filteredData = tableData.filter(data => data.datetime === inputValue);
+    var filteredData = tableData.filter(data => data.datetime === inputValue)
 
     // append html table with each listing
     filteredData.forEach((ufoSighting) => {
@@ -36,6 +38,20 @@ function handleSubmit() {
           cell.text(value);
         });
       });
+}
+
+    // if there isn't a value being searched, list all known values:
+else {
+  var filteredData = tableData.filter(data => data.datetime)
+  filteredData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
 };
     
 ///////////////////////////
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////
 /////////////////////// Assignment 14 - Javascript Challenge ///////////////////////
-///////////////////////////////// UFO - Level - 1 //////////////////////////////////
+///////////////////////////////// UFO - Level - 2 //////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -19,14 +19,21 @@ function handleSubmit() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
   
-    // Select the new input value from the form
+    // Select the new input values from the form
     var inputValue = d3.select("#datetime").property("value");
+    var inputCity = d3.select("#city").property("value");
   
     // clear the table to prepare for new data filter
     tbody.html("")
-  
+
+    // if there is a value being searched:
+if (inputValue){
     // filter the data based on the search
-    var filteredData = tableData.filter(data => data.datetime === inputValue);
+    var filteredData = tableData.filter(data => data.datetime === inputValue)
+    // var filterCity = filteredData.filter(data => data.city === inputCity)
+
+    console.log(filteredData)
+    // console.log(filterCity)
 
     // append html table with each listing
     filteredData.forEach((ufoSighting) => {
@@ -36,6 +43,21 @@ function handleSubmit() {
           cell.text(value);
         });
       });
+}
+
+    // if there isn't a value being searched:
+else {
+  var filteredData = tableData.filter(data => data.datetime)
+  filteredData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+d3.select(".form-control").html("");
 };
     
 ///////////////////////////
